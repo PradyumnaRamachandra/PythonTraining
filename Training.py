87d8ab7c55075606bea08aa68456d19c8edb2f7e
Pradyumna R <prady07@gmail.com>
2020-05-11 17:05:29 +0530

    Updated Training

---
+++ Training.py
@@ -0,0 +1,109 @@
+
+
+#Working with variables
+
+# str="Pradyumna"
+# num=25
+# num1=35.67
+#
+#
+# str="wwww.apple.com"
+#
+# num=45
+# num1=55.67
+#
+#
+# str="pradyumna"
+# str="c"
+#
+# num=45
+
+# Input and Output
+#
+# str="Pradyumna"
+# print(str)
+#
+# num=45
+# print(num)
+#
+# num1=45.50
+# print(num1)
+#
+# str="I like to have Coffee"
+# print(str)
+
+#Formatting
+#
+# print(1,2,3,4,5,sep="*")
+# #
+# # name="Pradyumna"
+# # company="DXC"
+# #
+# # print("My name is {},company is {}".format(name,company))
+#
+#
+# print("My name is {name},and i go to {school}".format(name="Samrudh",school="NPS"))
+
+
+# # Input
+# name=input("Please Enter the name :")
+# company=input("Please enter the company :")
+# print("My name is{name} and i work in {company}".format(name=name,company=company))
+
+#Working with Operators
+
+# Arithamtic Operators
+
+# x=25
+# y=15
+#
+# print(x+y)
+# print(x-y)
+# print(x%y)
+# print(x//y)
+# print(x*y)
+# print(x/y)
+#
+# # Comparison Operators
+#
+# print(x<y)
+# print(x>y)
+# print(x==y)
+# print(x>=y)
+# print(x<=y)
+# print(x!=y)
+#
+# #assignment Operators
+# x=1
+# y=1
+# x=x+1,
+# x+=1
+
+# #Logical Operators
+#
+# x=True
+# y=False
+#
+# print(x and y)
+# print(x or y )
+# print( not x)
+
+#Identity Opearators
+# is , is not
+
+# x=5
+# y=10
+# print( x is y )
+#
+# print(id(x))
+# print(id(y))
+
+# Membership Operators
+
+# in ,not in
+
+# str="Pradyumna"
+#
+# print('z' in str)
+# print('z' not in str)
+
